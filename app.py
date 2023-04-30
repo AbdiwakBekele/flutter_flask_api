18a018bff0f8bf8471e530d90981914eb49d37f2
AbdiwakB <abdiwakbek3226@gmail.com>
2023-04-30 11:17:26 +0300

    commit

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "Hello"
+
+if __name__ == "__main__":
+    print("Starting Python Flask Server for Testing")
+    app.run()
